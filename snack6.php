4d48fcd0d305e759aa58c3d8f86ae975f52e6dbb
Maurizio <proiettomaurizio97@gmail.com>
2021-12-20 17:24:49 +0100

    snack 6 complete

---
+++ snack6.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>snack 6</title>
+    <style>
+        #grey {
+            background-color: grey;
+        }
+
+        #green {
+            background-color: green;
+        }
+    </style>
+</head>
+<body>
+    <!-- ## Snack 6
+
+    Utilizzare questo array: https://pastebin.com/CkX3680A. Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde. 
+    -->
+    <?php
+        $db = [
+            'teachers' => [
+                [
+                    'name' => 'Michele',
+                    'lastname' => 'Papagni'
+                ],
+                [
+                    'name' => 'Fabio',
+                    'lastname' => 'Forghieri'
+                ]
+            ],
+            'pm' => [
+                [
+                    'name' => 'Roberto',
+                    'lastname' => 'Marazzini'
+                ],
+                [
+                    'name' => 'Federico',
+                    'lastname' => 'Pellegrini'
+                ]
+            ]
+        ];
+        $rectangleGrey = [];
+        $rectagleGreen = [];
+        $arrayKeys = array_keys($db);
+        for ($i=0; $i < count($arrayKeys); $i++) { 
+            for ($x=0; $x < count($db[$arrayKeys[$i]]) ; $x++) { 
+                if ($arrayKeys[$i] === 'teachers') {
+                    echo "<div id='grey'>". $db[$arrayKeys[$i]][$x]['name']. "<br>" . $db[$arrayKeys[$i]][$x]['lastname']. "<br>" . "</div>";
+                } else {
+                    echo "<div id='green'>". $db[$arrayKeys[$i]][$x]['name']. "<br>" . $db[$arrayKeys[$i]][$x]['lastname']. "<br>" . "</div>";
+                }
+            }
+        }
+    ?>
+</body>
+</html>
