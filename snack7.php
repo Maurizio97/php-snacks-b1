48b1a7541c341c04dfb468525fb5921785fb2148
Maurizio <proiettomaurizio97@gmail.com>
2021-12-20 17:56:55 +0100

    snack 7

---
+++ snack7.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>snack 7</title>
+</head>
+<body>
+    <!-- ## Snack 7
+
+    Creare un array contenente qualche alunno di un'ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. Stampare Nome, Cognome e la media dei voti di ogni alunno.
+    Good luck :party_parrot: 
+    -->
+    <?php
+        $class = [
+            [
+                'name' => 'Michele',
+                'surname' => ' Papagni',
+                'votes' => [8,6,4,5,7,3]
+            ],
+    
+            [
+                'name' => 'Fabio',
+                'surname' => 'Forghieri',
+                'votes' => [5,4,4,4,5,4]
+            ],
+
+            [
+                'name' => 'Roberto',
+                'surname' => 'Marazzini',
+                'votes' => [9,9,9,9,9.5,10]
+            ],
+        ];
+
+        foreach($student as $class){
+            var_dump($student);
+            $sumVote = 0;
+            foreach ($vote as $student['votes']) {
+                $sumVote = $sumVote + $vote;
+                
+            }
+            $averageVotes = $sumVote / count($student['votes']);
+            echo '<div>' . $student['name'] . '<br>' . $student['surname'] . '<br>' . $averageVotes . '</div>';
+        }
+    ?>
+    
+</body>
+</html>
