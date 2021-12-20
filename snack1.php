5fae082ab228ff4373e171dc57c432e9b70c803d
Maurizio <proiettomaurizio97@gmail.com>
2021-12-20 15:12:51 +0100

    add array matchs

---
+++ snack1.php
@@ -0,0 +1,50 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+ </head>
+ <body>
+     <!-- ## Snack 1
+
+    Creiamo un array contenente le partite di basket di un'ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+    -->
+    <?php
+        $matchs = [
+            [
+                homeSquad => "Detroit Pistons",
+                pointHome => 100,
+                visitors => "Miami Heat",
+                pointVisitors => 90
+            ],
+            [
+                homeSquad => "Memphis Grizzlies",
+                pointHome => 100,
+                visitors => "Portland Trail Blazers",
+                pointVisitors => 105
+            ],
+            [
+                homeSquad => "Sacramento Kings",
+                pointHome => 121,
+                visitors => "San Antonio Spurs",
+                pointVisitors => 114
+            ],
+            [
+                homeSquad => "Chicago Bulls",
+                pointHome => 115,
+                visitors => "Los Angeles Lakers",
+                pointVisitors => 110
+            ],
+            [
+                homeSquad => "Minnesota Timberwolves
+                ",
+                pointHome => 111,
+                visitors => "Dallas Mavericks",
+                pointVisitors => 105
+            ],
+        ]
+    ?>
+ </body>
+ </html>
